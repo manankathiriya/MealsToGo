90ef0e6f1e23bc885cd09b92da05b3c9fabcbd26
manankathiriya <manankathiriya2308@gmail.com>
2023-02-10 11:22:13 +0530

    authenticaton

---
+++ src/features/map/components/MapCalloutComponent.js
@@ -0,0 +1,6 @@
+import styled from "styled-components/native";
+import { CompactRestaurantInfoComponent } from "../../../components/restaurant/CompactRestaurantInfoComponent";
+
+export const MapCalloutComponent = ({ restaurant }) => {
+  return <CompactRestaurantInfoComponent restaurant={restaurant} />;
+};
